97a85992fdf7c984ca46c2851d4357a51cd84626
Ramesh Timsina <rtimsina@student.42berlin.de>
2024-07-18 20:49:17 +0200

    not working

--- srcs/requirements/mariadb/conf/init_gen.sh
+++ srcs/requirements/mariadb/conf/init_gen.sh
@@ -2,10 +2,19 @@
 
 WP_USER=$(cat /run/secrets/wp_admin_user)
 WP_PASS=$(cat /run/secrets/wp_admin_pass)
+DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
-cat <<EOF > /etc/mysql/init.sql
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS '$WP_USER'@'%' IDENTIFIED BY '$WP_PASS';
-GRANT ALL PRIVILEGES ON *.* TO '$WP_USER'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-EOF
+sleep 5
+echo "starting mariadb"
+service mariadb start
+
+mariadb -u root -p"$DB_ROOT_PASSWORD" <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS wordpress --allow-root;
+    CREATE USER IF NOT EXISTS '$WP_USER'@'%' IDENTIFIED BY '$WP_PASS';
+    GRANT ALL PRIVILEGES ON *.* TO '$WP_USER'@'%' WITH GRANT OPTION;
+    FLUSH PRIVILEGES;
+EOSQL
+echo "created table stopping mariadb"
+service mariadb stop
+sleep 5
+mysqld
--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -5,14 +5,19 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 
 chmod +x wp-cli.phar
 
-DB_USER=$(cat /run/secrets/db_user.txt)
-DB_PASS=$(cat /run/secrets/db_password.txt)
-WP_ADMIN_USER=$(cat /run/secrets/wp_admin_user.txt)
-WP_ADMIN_PASS=$(cat /run/secrets/wp_admin_pass.txt)
-WP_ADMIN_EMAIL=$(cat /run/secrets/wp_admin_email.txt)
+DB_USER=$(cat /run/secrets/db_user)
+DB_PASS=$(cat /run/secrets/db_password)
+WP_ADMIN_USER=$(cat /run/secrets/wp_admin_user)
+WP_ADMIN_PASS=$(cat /run/secrets/wp_admin_pass)
+WP_ADMIN_EMAIL=$(cat /run/secrets/wp_admin_email)
 
+echo "WAITING FOR MARIADB ...."
+sleep 10
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=newwordpress --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=localhost --title=inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --allow-root
-
+./wp-cli.phar plugin install --activate --path='/var/www/html' jetpack --allow-root
+./wp-cli.phar theme install twentytwenty --activate --path='/var/www/html' --allow-root
+echo "FINISHING SETTING UP WORDPRESS...."
+sleep 5
 php-fpm7.4 -F
